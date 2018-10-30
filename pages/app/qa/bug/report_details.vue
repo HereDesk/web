3386fa11ad933ac70b6a2ccfa38799200367c56f
小糊涂 <>
2018-10-30 12:00:11 +0800

    ui bug

--- pages/app/qa/bug/report_details.vue
+++ pages/app/qa/bug/report_details.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-xl-8 col-lg-10 offset-xl-2 offset-lg-10 col-md-12 col-sm-12 col-12">
+      <div class="col-xl-1 col-lg-10 offset-xl-1 offset-lg-1 col-md-12 col-sm-12 col-12">
         <div id="report_title" class="text-center mt-5">
           <h1>{{ Details.product_code }}日报</h1>
           <span>{{ Details.datetime }}</span>
