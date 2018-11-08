48d04696ab6d34adbc3b787997f5214194d8e35c
小糊涂 <>
2018-11-09 07:36:24 +0800

    if visitd fail, display error page

---
+++ layouts/error.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col text-center">
+        <h1 class="mb-5" style="margin-top:15%;">访问到了错误的页面</h1>
+        <nuxt-link to="/app/dashboard">首页</nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/error.vue
+++ pages/error.vue
@@ -8,7 +8,7 @@
       </div>
     </div>
     <div class="row justify-content-center mt-5">
-      <div class="col-4">
+      <div class="col-md-4 col-sm-12">
         <p>1. 访问出现错误，请确认访问请求</p>
         <p>2. 请检查是否已连接到网络,且网络畅通</p>
         <p>3. 若网络良好，请联系管理员检查服务器运行情况</p>
