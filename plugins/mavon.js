ef252ecdb6e390b39530d46d8372213c389dfcba
1211 <1111>
2018-12-07 16:17:01 +0800

    import mavon-markdown

---
+++ plugins/mavon.js
@@ -0,0 +1,6 @@
+// import Vue from 'vue'
+// import mavonEditor from 'mavon-editor'
+// import 'mavon-editor/dist/css/index.css'
+// Vue.use(mavonEditor)
+
+// <mavon-editor class="v-radius" placeholder="请输入实际结果 ~" :subfield="false" :toolbarsFlag="false" @change="mdChange('reality_result')"></mavon-editor>
