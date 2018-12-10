9d23c92650a9883235ab07062922c24aab64ae12
yi-heng <shanxi.1991@gmail.com>
2018-12-10 10:48:28 +0800

    Delete mavon.js

--- plugins/mavon.js
+++
@@ -1,6 +0,0 @@
-// import Vue from 'vue'
-// import mavonEditor from 'mavon-editor'
-// import 'mavon-editor/dist/css/index.css'
-// Vue.use(mavonEditor)
-
-// <mavon-editor class="v-radius" placeholder="请输入实际结果 ~" :subfield="false" :toolbarsFlag="false" @change="mdChange('reality_result')"></mavon-editor>
