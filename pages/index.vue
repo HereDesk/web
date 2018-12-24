c4d47388646da6112d215bc8482c694308a91d35
小糊涂 <>
2018-12-24 14:49:04 +0800

    优化代码风格

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,25 @@
 <template>
   <div id="page-user-login" class="container mt-5" v-cloak>
     <div class="row">
-      <div class="col-lg-12 col-md-12 col-sm-12 col-12" style="float:right;">
+      <div class="col">
         <h1>HDesk</h1>
       </div>
-      <div class="col-lg-12 col-md-12 col-sm-12 col-12 p-user-login-main">
-        <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12 offset-lg-3 offset-md-3 offset-xl-4">
-          <h2 class="p-user-login-title">欢迎登录</h2>
-          <div class="input-group input-group-lg" id="input-email">
-            <input type="email" id="user_name" name="email" class="form-control" 
-              maxlength="30" minlength="6" placeholder="电子邮件"
-              v-model="LoginData.username" />
-          </div>
-          <div class="input-group input-group-lg passwd" id="input-passwd">
-            <input type="password" id="user_passwd" name="user_passwd" class="form-control" 
-              maxlength="30" minlength="6" placeholder="输入密码" 
-              v-model="LoginData.password" @keyup.enter="goLogin" />
-          </div>
-          <div class="login-btn">
-            <button id="user_login_btn" class="btn btn-login btn-lg btn-block" type="button" @click="goLogin">登录</button>
-          </div>
+    </div>
+    <div class="row justify-content-center" style="margin-top:15%;">
+      <div class="col-lg-4 col-md-6 col-sm-8 col-12 text-center">
+        <h2 class="p-user-login-title">欢迎登录</h2>
+        <div id="input-email" class="input-group input-group-lg">
+          <input type="email" id="user_name" name="email" class="form-control" 
+            maxlength="30" minlength="6" placeholder="电子邮件"
+            v-model.trim="LoginData.username" />
+        </div>
+        <div id="input-passwd" class="input-group input-group-lg passwd">
+          <input type="password" id="user_passwd" name="user_passwd" class="form-control" 
+            maxlength="30" minlength="6" placeholder="输入密码" 
+            v-model.trim="LoginData.password" @keyup.enter="goLogin" />
+        </div>
+        <div class="login-btn">
+          <button id="user_login_btn" class="btn btn-login btn-lg btn-block" type="button" @click="goLogin">登录</button>
         </div>
       </div>
     </div>
@@ -49,9 +49,8 @@ export default {
   },
   methods: {
     goLogin() {
-      let username = this.LoginData.username
-      let password = this.LoginData.password
-      if (username.replace(/^s+/g, "").length === 0) {
+      let [username,password] = [this.LoginData.username,this.LoginData.password]
+      if (username.length === 0) {
         this.$notify.error({
           title: "提交失败",
           message: "用户名不为空哦"
@@ -65,7 +64,7 @@ export default {
         })
         return
       }
-      if (password.replace(/^s+/g, "").length === 0) {
+      if (password.length === 0) {
         this.$notify.error({
           title: "提交失败",
           message: "密码不能为空哦"
@@ -83,17 +82,16 @@ export default {
         method: "post",
         url: "/api/user/login",
         data: JSON.stringify(this.LoginData)
-      })
-        .then(res => {
+      }).then(res => {
           if (res.data["status"] === 10000) {
             // set localStorage and token
             var token = res.data["data"]["token"]
             if (process.browser) {
               window.localStorage.setItem("token", token)
-              var Days = 30;
-              var exp = new Date();
-              exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
-              document.cookie = "token=" + token + ";" + "expires=" + exp.toGMTString() + ";" + "path=/";
+              var Days = 30
+              var exp = new Date()
+              exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000)
+              document.cookie = "token=" + token + ";" + "expires=" + exp.toGMTString() + ";" + "path=/"
             }
             this.$store.commit("setUserInfo", res.data)
             if (res.data["data"]["group"] !== "admin") {
@@ -119,5 +117,5 @@ export default {
 </script>
 
 <style>
-  @import "../static/static/common/css/login.css";
+  @import "../static/static/common/css/login.css"
 </style>
