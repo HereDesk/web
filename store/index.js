f1d2f4ffd0848d237c64050300d3efa981d6f398
小糊涂 <>
2019-06-02 22:32:20 +0800

    优化 nuxt3.0状态树写法改变

--- store/index.js
+++ store/index.js
@@ -4,87 +4,71 @@ import axios from 'axios'
 
 Vue.use(Vuex)
 
-const createStore = () => {
-  return new Vuex.Store({
-    state: {
-      userInfo: false,
-      PageData: false,
-      UserConfig: [],
-      isLogin: false,
-      ProductInfo: [],
-      isProductInfo: false,
-      BugProperty: {},
-      ProductMemberList: {},
-      ProductVersionInfo: {},
-      ProductModulesInfo: {},
+export const state = () => ({
+    userInfo: false,
+    PageData: false,
+    UserConfig: [],
+    isLogin: false,
+    ProductInfo: [],
+    isProductInfo: false,
+    BugProperty: {},
+    ProductMemberList: {},
+    ProductVersionInfo: {},
+    ProductModulesInfo: {}
+})
+
+export const mutations = {
+    setProductVersionInfo(state, data) {
+        state.ProductVersionInfo = data
     },
-    mutations: {
-      setProductVersionInfo(state, data) {
-        if (data) {
-          state.ProductVersionInfo = data
-        }
-      },
-      setProductModulesInfo(state, data) {
-        if (data) {
-          state.ProductModulesInfo = data
-        }
-      },
-      setLoginInfo(state, data) {
-        if (data) {
-          state.isLogin = true
-        }
-      },
-      setUserInfo(state, data) {
-        if (data) {
-          state.userInfo = data['data']
-          state.UserConfig = data['config']
-        }
-      },
-      setPageData(state, data) {
-        if (data) {
-          state.PageData = data['data']
-        }
-      },
-      setProductInfo(state, data) {
-        if (data) {
-          state.isProductInfo = true
-          state.ProductInfo = data
-        }
-      },
-      setBugPropery(state, data) {
-        if (data) {
-          state.BugProperty = data
-        }
-      },
-      setProductMemberList(state,data) {
-        if (data) {
-          state.ProductMemberList = data
-        }
-      }
+    setProductModulesInfo(state, data) {
+        state.ProductModulesInfo = data
     },
-    actions: {
-      async getUserInfo({ commit }) {
-        const { data } = await axios.get('/api/user/userinfo')
-        commit('setUserInfo', data)
-      },
-      async getPageData({ commit },product_id) {
-        const { data } = await axios.get('/api/user/pages?product_id=' + product_id)
-        commit('setPageData', data)
-      },
-      async getProductRelease({ commit }) {
-        const { data } = await axios.get('/api/pm/product/my') 
-        commit('setProductInfo', data)
-      },
-      async getProductMembers({ commit },product_id) {
-        const { data } = await axios.get('/api/pm/member/list?product_id=' + product_id) 
-        commit('setProductMemberList', data)
-      },
-      async getBugProperty({ commit }) {
-        const { data } = await axios.get('/api/qa/bug/bug_property')
-        commit('setBugPropery', data)
-      }
+    setLoginInfo(state, data) {
+        state.isLogin = true
+    },
+    setUserInfo(state, data) {
+        if (data) {
+            state.userInfo = data['data']
+            state.UserConfig = data['config']
+        }
+    },
+    setPageData(state, data) {
+        if (data) {
+            state.PageData = data['data']
+        }
+    },
+    setProductInfo(state, data) {
+        state.isProductInfo = true
+        state.ProductInfo = data
+    },
+    setBugPropery(state, data) {
+        state.BugProperty = data
+    },
+    setProductMemberList(state, data) {
+        state.ProductMemberList = data
     }
-  })
 }
 
-export default createStore
+export const actions = {
+    async getUserInfo({ commit }) {
+        const { data } = await axios.get('/api/user/userinfo')
+        commit('setUserInfo', data)
+    },
+    async getPageData({ commit }, product_id) {
+        const { data } = await axios.get('/api/user/pages?product_id=' + product_id)
+        commit('setPageData', data)
+    },
+    async getProductRelease({ commit }) {
+        const { data } = await axios.get('/api/pm/product/my')
+        commit('setProductInfo', data)
+    },
+    async getProductMembers({ commit }, product_id) {
+        const { data } = await axios.get('/api/pm/member/list?product_id=' + product_id)
+        commit('setProductMemberList', data)
+    },
+    async getBugProperty({ commit }) {
+        const { data } = await axios.get('/api/qa/bug/bug_property')
+        commit('setBugPropery', data)
+    }
+}
